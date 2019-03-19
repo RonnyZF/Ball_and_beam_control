8564e70e65f929676d7f1c79fc3e6d10561e0cea
ronnyzf <ronnyzarate@gmail.com>
2019-03-19 10:54:31 -0600

    correccion tiempo impulso

--- PSoC_Code/Modelo_empírico.c
+++ PSoC_Code/Modelo_empírico.c
@@ -16,15 +16,15 @@ uint32 InterruptCnt;
 ********************************************************************************/
 CY_ISR(InterruptHandler)
 {
-    /* Read Status register in order to clear the sticky Terminal Count (TC) bit 
-     * in the status register. Note that the function is not called, but rather 
-     * the status is read directly.
-     */
-    Timer_1_STATUS;
-    
-    /* Increment the Counter to indicate the keep track of the number of 
+	/* Read Status register in order to clear the sticky Terminal Count (TC) bit
+	 * in the status register. Note that the function is not called, but rather
+	 * the status is read directly.
+	 */
+   	Timer_1_STATUS;
+
+	/* Increment the Counter to indicate the keep track of the number of
      * interrupts received */
-    InterruptCnt++;    
+    InterruptCnt++;
 }
 
 int main()
@@ -40,7 +40,7 @@ int main()
     uint8 ContinuouslySendData;
     /* Transmit Buffer */
     char TransmitBuffer[TRANSMIT_BUFFER_SIZE];
-    
+
     /* Enable the global interrupt */
     CyGlobalIntEnable;
     /* Enable the Interrupt component connected to Timer interrupt */
@@ -50,21 +50,21 @@ int main()
     UART_1_Start();
     Timer_1_Start();
     Timer_2_Start();
-    
+
     /* Initialize Variables */
     ContinuouslySendData = FALSE;
-    
+
     /* Start the ADC conversion */
     ADC_DelSig_1_StartConvert();
-    
+
     /* Send message to verify COM port is connected properly */
     UART_1_PutString("COM Port Open\r\n");
-    
+
     for(;;)
-    {        
+    {
         /* Non-blocking call to get the latest data recieved  */
         Ch = UART_1_GetChar();
-        
+
         /* Set flags based on UART command */
         switch(Ch)
         {
@@ -85,9 +85,9 @@ int main()
                 break;
             default:
                 /* Place error handling code here */
-                break;    
+                break;
         }
-        
+
         while(echo_Read()==0){
             trigger_out_Write(1);
             CyDelay(10u);
@@ -97,7 +97,7 @@ int main()
         while(echo_Read()==1){};
         value_counter= 65535-Timer_2_ReadCounter();
         distancia=value_counter/58;
-        
+
 
         /* Check to see if an ADC conversion has completed */
         if(ADC_DelSig_1_IsEndConversion(ADC_DelSig_1_RETURN_STATUS))
@@ -111,7 +111,7 @@ int main()
                     PWM_1_Start();
                     entrada=5;
                 }
-                else if((InterruptCnt==148)||(InterruptCnt==149)||(InterruptCnt==150)||(InterruptCnt==151)){
+                else if((InterruptCnt==98)||(InterruptCnt==99)||(InterruptCnt==100)||(InterruptCnt==101)){
                     PWM_1_Stop();
                     entrada=0;
                 }
